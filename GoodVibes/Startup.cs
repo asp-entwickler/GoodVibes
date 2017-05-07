3849aea81b687f1d00940264b92caef875ae5a40
Obolenskiy Sergey <asp.entwickler@gmail.com>
2017-05-07 12:38:42 +0300

    Initial Commit

---
+++ GoodVibes/Startup.cs
@@ -0,0 +1,14 @@
+﻿//using Microsoft.Owin;
+//using Owin;
+
+//[assembly: OwinStartupAttribute(typeof(GoodVibes.Startup))]
+//namespace GoodVibes
+//{
+//    public partial class Startup
+//    {
+//        public void Configuration(IAppBuilder app)
+//        {
+            
+//        }
+//    }
+//}
